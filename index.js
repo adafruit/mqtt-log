a4a83e2607e42fb620eb88411bb93629ed11a799
Kevin Townsend <microbuilder@users.noreply.github.com>
2015-12-30 05:20:36 +0100

    Added CORS headers to HTTP Server

--- index.js
+++ index.js
@@ -66,7 +66,12 @@ aedes.on('publish', function(packet, client) {
 });
 
 var web = http.createServer(function(req, res) {
-
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+	
   var topic = url.parse(req.url).pathname;
 
   if(topic === '/')
